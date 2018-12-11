87ecc4d18c80beaca927e9fdc55c11ab8a9bb40d
kashiash <kashiash@gmail.com>
2018-12-11 08:57:46 +0100

    initial version

---
+++ TestService/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace TestService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HostFactory.Run(x =>
+            {
+                x.Service<HttpApiService>(s =>
+                {                   
+                    s.ConstructUsing(name => new HttpApiService(new Uri("http://localhost:8080")));        
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+                x.StartManually();
+                x.SetDescription("Sample Web API Windows service");
+                x.SetDisplayName("Znachor.WebApiTopShelfService");
+                x.SetServiceName("Znachor.WebApiTopShelfService");
+            });
+        }
+    }
+}
---
+++ TestService/ServiceController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TestService
+{
+   public class ServiceController : ApiController
+    {
+
+        public string Hello()
+        {
+            return "Cześc co sie szczypiesz?";
+        }
+
+
+        //http://localhost:8080/api/service?tekst=dupa
+        public string get(string tekst)
+        {
+            return $"Hmm ... {tekst}? Nieźle niezle!";
+        }
+
+    }
+}
